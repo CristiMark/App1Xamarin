83f09f59eb97977957f9b6a4aa87f004a0371dcb
CristiMark-PC\Cristi Mark <cristiankeisz@hotmail.com>
2018-01-12 22:22:27 +0200

    Add project files.

---
+++ App1Xamarin/App1Xamarin/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace App1Xamarin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+
+			InitializeComponent();
+
+			ToASCII.Clicked += ToASCII_Clicked;
+			ToBinar.Clicked += ToBinar_Clicked;
+			ToText.Clicked += ToText_Clicked;
+			 
+		}
+
+		private void ToText_Clicked(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void ToBinar_Clicked(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void ToASCII_Clicked(object sender, EventArgs e)
+		{
+			string input = InputValue.Text;
+			string output = null;
+
+			input.ForEach(x => output += x);
+
+			//foreach (char c in input)
+			//{
+			//    Console.WriteLine(System.Convert.ToInt32(c));
+			//}
+			//Console.ReadLine();
+		}
+	}
+}
